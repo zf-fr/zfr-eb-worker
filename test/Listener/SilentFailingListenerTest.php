7748873a9f67c32d14316c54ddf680f4fe2ac271
Michaël Gallego <mic.gallego@gmail.com>
2017-06-26 17:43:18 +0200

    Fix

--- test/Listener/SilentFailingListenerTest.php
+++ test/Listener/SilentFailingListenerTest.php
@@ -2,7 +2,7 @@
 
 namespace ZfrEbWorkerTest\Listener;
 
-use Psr\Http\Message\ResponseInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use ZfrEbWorker\Listener\SilentFailingListener;
 
@@ -14,21 +14,10 @@ class SilentFailingListenerTest extends \PHPUnit_Framework_TestCase
     public function testReturnsSuccessResponse()
     {
         $request  = $this->prophesize(ServerRequestInterface::class);
-        $response = $this->prophesize(ResponseInterface::class);
         $listener = new SilentFailingListener();
 
-        // Creates a response with status code 200
-        $successResponse = $this->prophesize(ResponseInterface::class)->reveal();
+        $response = $listener->process($request->reveal(), $this->prophesize(DelegateInterface::class)->reveal());
 
-        $response->withStatus(200)->shouldBeCalled()->willReturn($successResponse);
-
-        // It should not call $next
-        $next = function () {
-            $this->fail('$next should not be called!');
-        };
-
-        $returnedResponse = $listener($request->reveal(), $response->reveal(), $next);
-
-        $this->assertSame($successResponse, $returnedResponse);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
